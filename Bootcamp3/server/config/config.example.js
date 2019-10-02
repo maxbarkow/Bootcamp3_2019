eb31bc211602061868bca99a1f893a6d7b595594
maxbarkow <maxbarkow@ufl.edu>
2019-10-02 13:44:01 -0400

    Bootcamp3 commit

--- Bootcamp3/server/config/config.example.js
+++ Bootcamp3/server/config/config.example.js
@@ -1,13 +1,13 @@
-//This file holds any configuration variables we may need 
+//This file holds any configuration variables we may need
 //'config.js' is ignored by git to protect sensitive information, such as your database's username and password
 //copy this file's contents to another file 'config.js' and store your MongoLab uri there
 
 module.exports = {
   db: {
-    uri: ''//place the URI of your mongo database here.
-  }, 
+    uri: 'mongodb+srv://maxbarkow:5sCe9DUTIdEjPy5z@cluster0-6mgad.mongodb.net/Bootcamp2?retryWrites=true&w=majority',
+  },
   openCage: {
-    key: '' //place your openCage public key here - Sign-up for a free key https://opencagedata.com/
+    key: '55108ffb58f14d84b942274fb9ef1854' //place your openCage public key here - Sign-up for a free key https://opencagedata.com/
   },
   port: 8080
 };
--- Bootcamp3/server/controllers/listings.server.controller.js
+++ Bootcamp3/server/controllers/listings.server.controller.js
@@ -1,21 +1,21 @@
 
 /* Dependencies */
-var mongoose = require('mongoose'), 
+var mongoose = require('mongoose'),
     Listing = require('../models/listings.server.model.js'),
     coordinates = require('./coordinates.server.controller.js');
-    
+
 /*
   In this file, you should use Mongoose queries in order to retrieve/add/remove/update listings.
-  On an error you should send a 404 status code, as well as the error message. 
+  On an error you should send a 404 status code, as well as the error message.
   On success (aka no error), you should send the listing(s) as JSON in the response.
 
-  HINT: if you are struggling with implementing these functions refer back to this tutorial 
+  HINT: if you are struggling with implementing these functions refer back to this tutorial
   https://www.callicoder.com/node-js-express-mongodb-restful-crud-api-tutorial/
   or
   https://medium.com/@dinyangetoh/how-to-build-simple-restful-api-with-nodejs-expressjs-and-mongodb-99348012925d
-  
 
-  If you are looking for more understanding of exports and export modules - 
+
+  If you are looking for more understanding of exports and export modules -
   https://www.sitepoint.com/understanding-module-exports-exports-node-js/
   or
   https://adrianmejia.com/getting-started-with-node-js-modules-require-exports-imports-npm-and-beyond/
@@ -30,11 +30,11 @@ exports.create = function(req, res) {
   /* save the coordinates (located in req.results if there is an address property) */
   if(req.results) {
     listing.coordinates = {
-      latitude: req.results.lat, 
+      latitude: req.results.lat,
       longitude: req.results.lng
     };
   }
- 
+
   /* Then save the listing */
   listing.save(function(err) {
     if(err) {
@@ -56,33 +56,79 @@ exports.read = function(req, res) {
 /* Update a listing - note the order in which this function is called by the router*/
 exports.update = function(req, res) {
   var listing = req.listing;
+if(!req.body){
+  return res.status(400).send(err);
+}
+Listing.findById(
+  listing.id,
+  function(err, listing) {
+    if(err)
+    res.status(400).send(err);
+});
 
+listing.code = req.body.code;
+listing.name = req.body.name;
+
+if(req.body.address)
+listing.address = req.body.address;
+
+if(req.results) {
+  listing.coordinates = {
+    latitude: req.results.lat,
+    longitude: req.results.lng
+  };
+}
+
+listing.save(function (err) {
+        if (err){
+              res.status(400).send(err);
+            }else{
+              res.json(listing);
+            }
+            });
+
+    };
   /* Replace the listings's properties with the new properties found in req.body */
- 
+
   /*save the coordinates (located in req.results if there is an address property) */
- 
+
   /* Save the listing */
 
-};
+
 
 /* Delete a listing */
 exports.delete = function(req, res) {
   var listing = req.listing;
 
-  /* Add your code to remove the listins */
+  Listing.remove({
+        _id: req.params.listing
+    }, function (err, listing) {
+        if (err)
+            res.send(err);
+res.json(listing);
+    });
+
+  /* Add your code to remove the listings */
 
 };
 
 /* Retreive all the directory listings, sorted alphabetically by listing code */
 exports.list = function(req, res) {
-  /* Add your code */
+
+  Listing.find().exec(function(err, listing) {
+    if(err) {
+      res.status(400).send(err);
+    } else {
+      req.listing = listing;
+      res.json(listing)
+    }
+  });
 };
+/*
+  Middleware: find a listing by its ID, then pass it to the next request handler.
 
-/* 
-  Middleware: find a listing by its ID, then pass it to the next request handler. 
-
-  HINT: Find the listing using a mongoose query, 
-        bind it to the request object as the property 'listing', 
+  HINT: Find the listing using a mongoose query,
+        bind it to the request object as the property 'listing',
         then finally call next
  */
 exports.listingByID = function(req, res, next, id) {
